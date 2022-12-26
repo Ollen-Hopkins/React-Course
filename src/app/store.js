c6b122b77f2b4e317982ace83eb5ea23672028bc
Ollen Hopkins <hopkins_ollen@yahoo.com>
2022-12-26 12:18:12 -0500

    Redux: Dispatching Actions & Adding Middleware

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { campsitesReducer } from '../features/campsites/campsitesSlice';
 import { commentsReducer } from '../features/comments/commentsSlice';
 import { partnersReducer } from '../features/partners/partnersSlice';
 import { promotionsReducer } from '../features/promotions/promotionsSlice';
+import logger from 'redux-logger';
+import { getDefaultNormalizer } from '@testing-library/react';
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +12,6 @@ export const store = configureStore({
     comments: commentsReducer,
     partners: partnersReducer,
     promotions: promotionsReducer
-  }
+  }, 
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([logger])
 });
