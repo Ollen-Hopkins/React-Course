8a4bbb5ccab70f177e5c4755ca97df12666e1876
Ollen Hopkins <hopkins_ollen@yahoo.com>
2023-01-06 08:28:32 -0500

    Post comments to campsite

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import Footer from './components/Footer';
 import AboutPage from './pages/AboutPage';
 import { fetchPartners } from './features/partners/partnersSlice';
 import { fetchPromotions } from './features/promotions/promotionsSlice';
+import { fetchComments } from './features/comments/commentsSlice';
 
 
 
@@ -23,6 +24,7 @@ function App() {
     dispatch(fetchCampsites());
     dispatch(fetchPartners());
     dispatch(fetchPromotions());
+    dispatch(fetchComments());
   }, [dispatch]);
 
 
--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,25 +1,70 @@
-import { COMMENTS } from '../../app/shared/COMMENTS';
+// import { COMMENTS } from '../../app/shared/COMMENTS';
 import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { baseUrl } from '../../app/shared/baseUrl';
+
+export const fetchComments = createAsyncThunk(
+    'comments/fetchComments',
+    async() => {
+        const response = await fetch(baseUrl + 'comments')
+        if(!response.ok){
+            return Promise.reject('Unable to fetch, status: ' + response.status);
+        }
+        const data = await response.json();
+        return data;
+    }
+)
+
+export const postComment = createAsyncThunk(
+    'comments/postComment',
+    async (comment, {dispatch}) => {
+        const response = await fetch(baseUrl + 'comments', {method: 'POST', body: JSON.stringify(comment), headers: {'Content-Type': 'application/json'}})
+        
+        if(!response.ok){
+            return Promise.reject(response.status);
+         } 
+         const data = await response.json();
+         dispatch(addComment(data));
+    }
+)
+// dispatch passing in addComment causes an 500 error, addComment function not defined.
+// dispatch postComment 
+
+
 
 const initialState = {
-    commentsArray: COMMENTS
-};
+    commentsArray: [],
+    isLoading: true,
+    errMsg: ''
+}
 
 const commentsSlice = createSlice({
     name: 'comments',
     initialState,
-    reducers: {
-        addComment: (state, action) => {
-            console.log('addComment action.payload:', action.payload);
-            console.log('addComment state.commentsArray:', state.commentsArray);
-            const newComment = {
-                id: state.commentsArray.length + 1,
-                ...action.payload
-            };
-            state.commentsArray.push(newComment);
-        }
+    reducers: [],
+    extraReducers: {
+        [fetchComments.pending]: (state) => {
+            state.loading = true;
+        },
+        [fetchComments.fulfilled]: (state, action) => {
+            state.isLoading = false;
+            state.errMsg = '';
+            state.commentsArray = action.payload;
+        },
+        [fetchComments.rejected]: (state, action) => {
+            state.isLoading = false;
+            state.errMsg = action.error ? action.error.message : 'Fetch failed';
+        },
+        reducer: {[postComment.rejected]: (state, action) => {
+            alert(
+                'Your comment could not be posted\nError: ' + 
+                    (action.error ? action.error.message : 'Fetch failed')
+            );
+        }}
     }
 });
+
+// Bug comment only posts when page is refreshed.
 
 export const commentsReducer = commentsSlice.reducer;
 
